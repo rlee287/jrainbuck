3f75cfa6377556d59e9a1f5eda36130bbce4ec64
rlee287 <rlee287@yahoo.com>
2016-11-11 13:09:36 -0800

    Rudimentary file reading and mapping

--- Jrainbuck/src/io/github/rlee287/jrainbuck/Main/JrainBuck.java
+++ Jrainbuck/src/io/github/rlee287/jrainbuck/Main/JrainBuck.java
@@ -4,17 +4,17 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 
-import io.github.rlee287.jrainbuck.constants.SwitchList;
+import io.github.rlee287.jrainbuck.constants.Constants;
+import io.github.rlee287.jrainbuck.uppering.UVBCreator;
+import io.github.rlee287.jrainbuck.uppering.UVCreator;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
 
 public class JrainBuck {
 
 	public static void main(String[] args) {
-		/*System.out.println("List of Arguments");
-		for (String a : args){
-			System.out.println(a);
-		}*/
 		HashMap <String,String> arguments=CommandParse(args);
 		if (arguments==null) {
 			System.err.println("Try using -help for more information.");
@@ -25,6 +25,8 @@ public class JrainBuck {
 			System.runFinalization();
 			System.exit(0);
 		}
+		UVCreator uppering=new UVBCreator(arguments.get("file"));
+		byte[] test=uppering.getUV();
 	}
 
 	public static HashMap <String,String> CommandParse(String[] args) {
@@ -36,10 +38,10 @@ public class JrainBuck {
 			return Arguments;
 		}
 		System.err.println();
-		for ( String Switch : SwitchList.LIST_SWITCHES) {
+		for ( String Switch : Constants.LIST_SWITCHES) {
 			int IndexFile=ArgumentsAsList.indexOf(Switch);
 			if (IndexFile==-1) {
-				if (SwitchList.LIST_SWITCHES[0]==Switch){
+				if (Constants.LIST_SWITCHES[0]==Switch){
 					System.err.println("Error: A file must be specified with "+Switch);
 					return null;
 				}
