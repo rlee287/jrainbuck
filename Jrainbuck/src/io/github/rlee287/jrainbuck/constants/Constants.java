1e2ddb57be198e2fe0257757fd96d6f2252bad3e
rlee287 <rlee287@yahoo.com>
2016-11-12 22:48:11 -0800

    Add array size and mapping for max delta compression of UVC

--- Jrainbuck/src/io/github/rlee287/jrainbuck/constants/Constants.java
+++ Jrainbuck/src/io/github/rlee287/jrainbuck/constants/Constants.java
@@ -30,9 +30,23 @@ public class Constants {
 		/* STDIN 0b0001 0001*/
 		INIT_UVB_MAP.put(',', new Byte((byte)17));
 	}
+	private static HashMap<Character, Byte> INIT_UVC_MAX=
+			new HashMap <Character, Byte>();
+	static {
+		INIT_UVC_MAX.put('[', new Byte((byte)63));
+		INIT_UVC_MAX.put(']', new Byte((byte)63));
+		INIT_UVC_MAX.put('+', new Byte((byte)31));
+		INIT_UVC_MAX.put('-', new Byte((byte)31));
+		INIT_UVC_MAX.put('<', new Byte((byte)15));
+		INIT_UVC_MAX.put('>', new Byte((byte)15));
+		INIT_UVC_MAX.put('.', new Byte((byte)7));
+		INIT_UVC_MAX.put(',', new Byte((byte)7));
+	}
 	public static final HashMap<Character,Byte> UVB_MAP=INIT_UVB_MAP;
+	public static final HashMap<Character,Byte> UVC_MAX=INIT_UVC_MAX;
 	/*
 	 * 0 |0 |0 |0 |0 |0 |0 |0
 	 * []|+-|<>|.,|00|00|00|sign
 	 */
+	public static final int ARRAY_SIZE = 10000;
 }
